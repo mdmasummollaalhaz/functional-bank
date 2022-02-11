32c271a1567f52d1823d8b81ab8b6dac10ca7a37
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-11 17:31:18 +0600

    deposit

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,19 @@
-alert('jj')
+document.getElementById('deposit-btn').addEventListener('click', function(){
+    // console.log('Clicked');
+
+    const depositeInput = document.getElementById('deposit-input');
+    const depositeAmount = depositeInput.value;
+    // console.log(depositeAmount);
+
+    // get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositeTotalText = depositTotal.innerText;
+    depositTotal.innerText = depositeAmount;
+    console.log(depositeTotalText);
+
+    // Clear input number
+    depositeInput.value = '';
+    
+    
+    
+})
