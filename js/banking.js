8887ea60a4c21e8a39374ce56ae136f626afe103
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-12 00:48:14 +0600

    withdraw

--- js/banking.js
+++ js/banking.js
@@ -25,3 +25,31 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     depositeInput.value = '';
     
 });
+
+
+// Handle withdraw 
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    // console.log('Clicked');
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    // console.log(withdrawAmountText);
+    const widthdrawAmount = parseFloat(withdrawAmountText);
+
+    // Update withdraw total
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + widthdrawAmount;
+
+
+    // update balance after withdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - widthdrawAmount;
+
+    // Clear widthdraw input field
+    withdrawInput.value = '';
+    
+});
